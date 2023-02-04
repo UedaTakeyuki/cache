1cefcae58badfc39eadfe0736e7935085d84da83
Takeyuki UEDA <ueda@latelierdueda.com>
2023-02-05 08:30:31 +0900

    add test

---
+++ test/cache_test.go
@@ -0,0 +1,37 @@
+package cache
+
+import (
+	"testing"
+
+	cp "github.com/UedaTakeyuki/compare"
+	"local.packages/cache"
+)
+
+func Test_01(t *testing.T) {
+	c, err := cache.NewCache(3, true)
+	cp.Compare(t, err, nil)
+	{
+		val := c.AddOrReplace(1, "a")
+		cp.Compare(t, val, "a")
+	}
+	c.AddOrReplace(2, "b")
+	c.AddOrReplace(3, "c")
+	{
+		val, exist := c.Get(1)
+		cp.Compare(t, val, "a")
+		cp.Compare(t, exist, "true")
+	}
+	c.AddOrReplace(4, "d")
+	{
+		_, exist := c.Get(1)
+		// should be exist
+		cp.Compare(t, exist, "true")
+		// should not be exist, already deleted.
+		_, exist = c.Get(2)
+		cp.Compare(t, exist, "false")
+		val, _ := c.Get(4)
+		cp.Compare(t, val, "d")
+	}
+	c.AddOrReplace(5, "e")
+
+}
---
+++ test/go.mod
@@ -0,0 +1,10 @@
+module github.com/UedaTakeyuki/cache/test
+
+go 1.19
+
+replace local.packages/cache => ../../cache
+
+require (
+	github.com/UedaTakeyuki/compare v0.0.0-20221125001138-707873cbc2cb
+	local.packages/cache v0.0.0-00010101000000-000000000000
+)
